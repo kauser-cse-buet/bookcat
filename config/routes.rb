39a7a0f1a27f83b75ee1c41125ae40fcc43932e5
Scott Fleming <Scott.Fleming@memphis.edu>
2016-02-01 19:52:49 +0000

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+
+  get 'about', to: 'static_pages#about', as: 'about'
+  get 'home', to: 'static_pages#home', as: 'home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
